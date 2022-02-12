f4f3c4e8b5c52d22505aba5550568a60ba9e2bce
Samantha Kosman <skosman@uvic.ca>
2022-02-11 23:10:55 -0800

    Add example pin to map

--- src/screens/Map/MapScreen.tsx
+++ src/screens/Map/MapScreen.tsx
@@ -1,10 +1,18 @@
 import MapView from 'react-native-maps';
 import {View, StyleSheet, Dimensions } from 'react-native';
+import { Marker } from 'react-native-maps';
+
+const exampleMarker = {
+  latitude: 48.463708,
+  longitude: -123.311406,
+};
 
 export const MapScreen= () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}>
+        <Marker coordinate={exampleMarker}></Marker>
+        </MapView>
     </View>
   );
 }
