e0662c5f6222214e77065100bd975a3248abfe53
Micetich <53983130+brendanmicetich@users.noreply.github.com>
2022-02-16 13:50:37 -0800

    create documentation for updated interfaces

--- src/data/Interfaces.ts
+++ src/data/Interfaces.ts
@@ -1,17 +1,10 @@
 import { GeoPoint } from 'firebase/firestore/lite';
 
 interface IPin {
-    readonly location: GeoPoint;
-    details: IPinDetails;
+  // https://firebase.google.com/docs/reference/node/firebase.firestore.GeoPoint
+  readonly location: GeoPoint;
+  details: IPinDetails;
 }
-
-// interface ILocation {
-//     readonly latitude: number;
-//     readonly longitude: number;
-
-//     isEqual(other: ILocation): boolean;
-//     toString (): string;
-// }
 
 interface IPinDetails {
     description: string;
@@ -20,39 +13,60 @@ interface IPinDetails {
 }
 
 interface IDatabase {
-  // attempts to add a pin at a given location.
-  // returns an IDatabaseAction result.
-  // will fail if a pin already exists
+  /* Purpose: attempts to add a pin at the given location.
+   * Return: an IDatabaseActionResult where success==true if the pin was
+   *         was added at location.
+   */
   addPin(pin: IPin): Promise<IDatabaseActionResult>;
 
-  // attempts to edit a pin at a given location, replacing existing details with provided details.
-  // returns an IDatabaseAction result.
-  // will fail if no pin exists
-  editPinDetails(pin: GeoPoint, details: IPinDetails): Promise<IDatabaseActionResult>;
+  /* Purpose: attempts to edit a pin at the given location, replacing existing details with provided details.
+   *          Will fail if no pin is found at location.
+   * Return: an IDatabaseActionResult where success==true if the pin at location was deleted
+   *         or success==false otherwise
+   */
+  editPinDetails(
+    pin: GeoPoint,
+    details: IPinDetails
+  ): Promise<IDatabaseActionResult>;
 
-  // attempts to remove the pin at a given location.
-  // returns an IDatabaseAction result.
+  /* Purpose: attempts to delete a pin at the given location.
+   *          Will fail if no pin is found at location.
+   * Return: an IDatabaseActionResult where success==true if the pin at location was deleted
+   *        or false otherwise
+   */
   deletePin(location: GeoPoint): Promise<IDatabaseActionResult>;
 
-  // returns a pin by its location
+  /* Purpose: attempts to get a pin from the given location.
+   *          Will fail if no pin is found at location.
+   * Return: an IPinActionResult<IPin> that will contain an IPin on success (undefined on failure)
+   */
   getPin(location: GeoPoint): Promise<IPinActionResult<IPin>>;
 
-  // returns a list of all pins
+  /* Purpose: attempts to get an array of all pins from the database.
+   * Return: an IPinActionResult<IPin[]> that will contain an IPin[] on success (undefined on failure)
+   */
   getAllPins(): Promise<IPinActionResult<IPin[]>>;
 
-  // returns a Location[] containing all pin locations
+  /* Purpose: attempts to get an array of all pin locations (GeoPoints) from the database.
+   * Return: an IPinActionResult<GeoPoint[]> that will contain a GeoPoint[] on success (undefined on failure)
+   */
   getAllPinLocations(): Promise<IPinActionResult<GeoPoint[]>>;
 }
 
-// succeeded returns true if the action was complete, false otherwise
-// message contains information about the result (such as why an action failed)
+/* 
+ * Purpose: used as a return value for database access functions to provide info about the result
+ * succeeded: true if action completed without error, false otherwise
+ * message: Error message
+*/
 interface IDatabaseActionResult {
     succeeded: boolean;
     message: string;
 }
 
-// returns the result of attemping to get a pin
-// returns the pin on success, or an empty pin on failure
+/*
+ * Purpose: contains IDatabaseActionResult info (succeeded and message)
+ * data: contains data of specified type if succeeded, otherwise undefined
+*/
 interface IPinActionResult<T> extends IDatabaseActionResult {
     data?: T;
 }
