58b83451715ccfaddbe9d9e8eeb7ea2b3521f6df
Micetich <53983130+brendanmicetich@users.noreply.github.com>
2022-02-16 10:10:50 -0800

    add ActionResult return types and error checking

--- src/data/Database.ts
+++ src/data/Database.ts
@@ -1,87 +1,130 @@
 import { firebaseApp } from "../config/FirebaseConfig"
-import { getFirestore, collection, getDocs, setDoc, doc, Firestore, getDoc, updateDoc, query, DocumentData, QueryDocumentSnapshot, deleteDoc, where } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, setDoc, doc, getDoc, updateDoc, QueryDocumentSnapshot, deleteDoc } from 'firebase/firestore/lite';
 import { Pin, Location, PinDetails } from "./Pin"
-import { IPin, ILocation, IPinDetails } from "./Interfaces"
+import { IPin, ILocation, IPinDetails, IDatabaseActionResult, IPinActionResult } from "./Interfaces"
 
 const database = getFirestore(firebaseApp);
 
 // Adds a pin to the database
-async function addPin (pin: IPin) {
+async function addPin (pin: IPin) : Promise<IDatabaseActionResult> {
     const pinRef = doc(database, "pins", pin.location.toString());
 
-    const pinDocSnap = await getDoc(pinRef);
+    try {
+        const pinDocSnap = await getDoc(pinRef);
 
         if (pinDocSnap.exists()) {
-            console.log("could not place pin at location: " + pin.location + ". Because pin already exists");
-            return
+            throw new Error(`Pin already exists.`);
         }
 
-    await setDoc(pinRef, pinConverter.toFirestore(pin));
-}
+        await setDoc(pinRef, pinConverter.toFirestore(pin));
 
-// Edit pin details at location
-async function editPinDetails (location: ILocation, details: PinDetails) {
-
-    const pinRef = doc(database, "pins", location.toString());
-
-    const pinDocSnap = await getDoc(pinRef);
-
-    if (!pinDocSnap.exists()) {
-        console.log("could not edit pin at location: " + location + ". Because pin does not exist");
-        return
+    } catch(error) {
+        return new DatabaseActionResult(false, `Failed: could not place pin at location: ${pin.location}. ${error}`);
     }
 
-    await updateDoc(pinRef, { details: pinDetailsConverter.toFirestore(details)});
+    return new DatabaseActionResult(true, `Succeeded: pin added at ${pin.location}`);
 }
 
-// deletes pin at given location
-async function deletePin (location: Location) {
-    await deleteDoc(doc(database, "pins", location.toString()));
+// Edits pin details at location
+async function editPinDetails (location: ILocation, details: PinDetails) : Promise<IDatabaseActionResult> {
+
+    try {
+        const pinRef = doc(database, "pins", location.toString());
+        const pinDocSnap = await getDoc(pinRef);
+
+        if (!pinDocSnap.exists()) {
+            throw new Error(`Pin could not be found.`);
+        }
+
+        await updateDoc(pinRef, { details: pinDetailsConverter.toFirestore(details)});
+
+    } catch(error) {
+
+        return new DatabaseActionResult(false, `Failed: could not edit pin at location ${location}. ${error}`);
+
+    }
+
+    return new DatabaseActionResult(true, `Succeeded: pin edited at ${location}`);
+}
+
+// Deletes pin at given location
+async function deletePin (location: Location) : Promise<IDatabaseActionResult> {
+    try {
+        await deleteDoc(doc(database, "pins", location.toString()));
+
+    } catch(error) {
+
+        return new DatabaseActionResult(false, `Failed: could not delete pin at location ${location}. ${error}`);
+
+    }
+
+    return new DatabaseActionResult(true, `Succeeded: pin deleted at: ${location}`);
 }
 
 // Get the pin at a given location
-async function getPin (location: Location) {
-    const pinRef = doc(database, "pins", location.toString());
+async function getPin (location: Location): Promise<IPinActionResult<IPin>> {
+    try {
+        const pinRef = doc(database, "pins", location.toString());
 
-    const pinDocSnap = await getDoc(pinRef);
+        const pinDocSnap = await getDoc(pinRef);
 
-    if (pinDocSnap.exists()) {
-        return pinConverter.fromFirestore(pinDocSnap);
+        if (!pinDocSnap.exists()) {
+            throw new Error(`Pin could not be found`);
+        }
+
+        const pin = pinConverter.fromFirestore(pinDocSnap);
+        
+        return new PinActionResult<IPin>(new DatabaseActionResult(true, `Succeeded: pin retrieved from ${location}`), pin);
+        
+    } catch(error){
+        return new PinActionResult<IPin>(new DatabaseActionResult(false, `Failed: pin could not be retrieved from ${location}. ${error}`), undefined);
     }
-    
-    console.log("could not find data at location: " + location.toString());
 }
 
 // Get a pin[] of all pins from the database
-async function getAllPins () {
-    const pinsCollection = collection(database, 'pins').withConverter(pinConverter);
+async function getAllPins (): Promise<IPinActionResult<IPin[]>> {
+    try {
+        const pinsCollection = collection(database, 'pins').withConverter(pinConverter);
 
-    const pinSnapshot = await getDocs(pinsCollection)
+        const pinSnapshot = await getDocs(pinsCollection)
 
-    const pinsList: Pin[] = [];
+        const pinsList: Pin[] = [];
+        
+        // converts each document into a pin object
+        pinSnapshot.forEach(pin => {
+            pinsList.push(pin.data());
+        });
+        
+        return new PinActionResult<IPin[]>(new DatabaseActionResult(true, `Succeeded: pins retrieved from ${location}`), pinsList);
 
-    pinSnapshot.forEach(pin => {
-        pinsList.push(pin.data());
-    });
-    
-    return pinsList;
+    } catch(error) {
+        return new PinActionResult<IPin[]>(new DatabaseActionResult(false, `Failed: pins could not be retrieved from ${location}. ${error}`), undefined);
+    }
 }
 
 // Get a Location[] of all pins from the database without retreiving details
 // Use getPin to get a specific pin
-async function getAllPinLocations () {
-    const pinsCollection = collection(database, 'pins');
+async function getAllPinLocations (): Promise<IPinActionResult<ILocation[]>> {
+    try {
+        const pinsCollection = collection(database, 'pins');
 
-    const pinSnapshot = await getDocs(pinsCollection)
+        const pinSnapshot = await getDocs(pinsCollection)
 
-    const pinsList = pinSnapshot.docs.map(doc => doc.id);
-    const pinLocationsList: Location[] = [];
+        const pinsList = pinSnapshot.docs.map(doc => doc.id);
 
-    pinsList.forEach(pin => {
-        pinLocationsList.push(getLocationFromString(pin));
-    });
-    
-    return pinLocationsList;
+        const pinLocationsList: Location[] = [];
+
+        pinsList.forEach(pin => {
+            pinLocationsList.push(getLocationFromString(pin));
+        });
+        
+        return new PinActionResult<ILocation[]>(new DatabaseActionResult(true, `Succeeded: pin locations retrieved from ${location}`), pinLocationsList);
+
+    } catch(error) {
+
+        return new PinActionResult<ILocation[]>(new DatabaseActionResult(false, `Failed: pin locations could not be retrieved from ${location}. ${error}`), undefined);
+
+    }
 }
 
 // data converters to transform data to and from json objects for firestore use
@@ -103,7 +146,6 @@ const pinDetailsConverter = {
   },
 };
 
-// Firestore location data converter
 const pinLocationConverter = {
   toFirestore: (location: ILocation) => {
     return {
@@ -139,6 +181,29 @@ function getLocationFromString(locationString: string): ILocation {
     const splitLocation: string[] = locationString.split(",", 2);
 
     return new Location(Number(splitLocation[0]), Number(splitLocation[1]));
+}
+
+// action result implementations
+class DatabaseActionResult implements IDatabaseActionResult {
+    readonly succeeded: boolean;
+    readonly message: string;
+
+    constructor(succeeded: boolean, message: string) {
+        this.succeeded = succeeded;
+        this.message = message;
+    }
+}
+
+class PinActionResult<T> implements IPinActionResult<T> {
+    succeeded: boolean;
+    message: string;
+    data?: T;
+
+    constructor(result: IDatabaseActionResult, data?: T) {
+        this.succeeded = result.succeeded;
+        this.message = result.message;
+        this.data = data;
+    }
 }
 
  export { addPin, editPinDetails, deletePin, getPin, getAllPins, getAllPinLocations };
