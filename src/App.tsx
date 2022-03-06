aa9243c10708a1bc7e0f2f368c2ae3f2c32cf452
Micetich <53983130+brendanmicetich@users.noreply.github.com>
2022-03-06 13:38:51 -0800

    Remove database test calls at startup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { TestAll } from "./tests/Database";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -7,8 +6,6 @@ import { store } from "./redux/Store";
 import { Provider } from "react-redux";
 
 export default function App() {
-  TestAll();
-
   return (
     <Provider store={store}>
       <SafeAreaProvider>
