135a1a763de87131e4bc095f64a74f6d96c2da0e
Samantha Kosman <skosman@uvic.ca>
2022-03-08 13:18:30 -0800

    Remove PinInfoCard from Friends screen

--- src/screens/Friends/FriendsScreen.tsx
+++ src/screens/Friends/FriendsScreen.tsx
@@ -1,16 +1,10 @@
 import { Text, View } from "react-native";
-import PinInfoCard from "../../components/PinInfoCard";
-import BottomDrawer from "react-native-bottom-drawer-view";
 
 export const FriendsScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Friends is under construction!</Text>
       <Text>No friends yet 😢</Text>
-
-      <BottomDrawer containerHeight={400} offset={49}>
-        <PinInfoCard />
-      </BottomDrawer>
     </View>
   );
 };
